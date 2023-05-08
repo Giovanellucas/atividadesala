597df48b3d6daca025b7b4c645fd1985cca4d036
Giovanellucas <129345132+Giovanellucas@users.noreply.github.com>
2023-05-08 20:53:42 -0300

    Create tde.js

---
+++ tde.js
@@ -0,0 +1,40 @@
+//1.
+for(let numero=0;numero<=100;numero++){
+        if(numero%2 === 0){
+            console.log(numero);
+        }
+    }
+
+
+
+//2.
+let nume=[2,4,6,8,10];
+let somaa = 0;
+
+for(let i=0;i<num.length;i++){
+    soma += num[i];
+}
+console.log(soma)
+
+
+
+
+//3.
+let numr=[-6,-3,-8,-1,2,4,6,8,10];
+let numremove = num.filter(num => num>=0);
+let soma = 0;
+
+console.log(numremove);
+
+
+
+
+//4.
+let num=[2,4,6,8,10];
+let mult=[];
+
+for(let i=0;i<
+    num.length;i++){
+    mult.push(num[i]*2);
+}
+console.log(mult);
